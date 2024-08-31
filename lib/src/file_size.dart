952d23b0a22be6b3cc32127804775ad3c03f6db6
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-08-31 17:13:06 +0100

    refactor: Split the implementation into functions.

--- lib/src/file_size.dart
+++ lib/src/file_size.dart
@@ -28,29 +28,56 @@ String fileSizeAsHuman(
 
   final inputBits = inputUnit.quantityToBits(quantity);
 
-  final outputUnit = switch (unitConversion) {
-    BestFitConversion(numeralSystem: final numeralSystem) =>
-      Unit.matchToSize(bits: inputBits, numeralSystem: numeralSystem),
-    CustomUnitConversion(unit: final unit) => unit,
-  };
+  final outputUnit =
+      _getOutputUnit(inputBits: inputBits, unitConversion: unitConversion);
   final outputQuantity = outputUnit.bitsToQuantity(inputBits);
 
-  final formattedUnit = switch (unitStyle) {
-    UnitStyle.shortLowercase => outputUnit.symbols.shortLowercase,
-    UnitStyle.shortUppercase => outputUnit.symbols.shortUppercase,
-    UnitStyle.longLowercase => outputUnit.symbols.longLowercase,
-    UnitStyle.longUppercase => outputUnit.symbols.longUppercase,
-  };
-  final formattedQuantity = switch (quantityDisplayMode) {
-    FixedPrecisionDisplayMode(precision: final precision) =>
-      outputQuantity.toStringAsFixed(precision),
-    PrecisionDisplayMode(precision: final precision) =>
-      outputQuantity.toStringAsPrecision(precision),
-    TruncateDisplayMode() => outputQuantity.truncate(),
-    RoundDisplayMode() => outputQuantity.round(),
-    CustomQuantityDisplayMode(converter: final converter) =>
-      converter(outputQuantity, unit: outputUnit),
-  };
+  final formattedUnit = _formatUnit(
+    outputUnit: outputUnit,
+    unitStyle: unitStyle,
+  );
+  final formattedQuantity = _formatQuantity(
+    outputUnit: outputUnit,
+    outputQuantity: outputQuantity,
+    quantityDisplayMode: quantityDisplayMode,
+  );
 
   return '$formattedQuantity $formattedUnit';
 }
+
+Unit _getOutputUnit({
+  required BigInt inputBits,
+  required UnitConversion unitConversion,
+}) =>
+    switch (unitConversion) {
+      BestFitConversion(numeralSystem: final numeralSystem) =>
+        Unit.matchToSize(bits: inputBits, numeralSystem: numeralSystem),
+      CustomUnitConversion(unit: final unit) => unit,
+    };
+
+String _formatUnit({
+  required Unit outputUnit,
+  required UnitStyle unitStyle,
+}) =>
+    switch (unitStyle) {
+      UnitStyle.shortLowercase => outputUnit.symbols.shortLowercase,
+      UnitStyle.shortUppercase => outputUnit.symbols.shortUppercase,
+      UnitStyle.longLowercase => outputUnit.symbols.longLowercase,
+      UnitStyle.longUppercase => outputUnit.symbols.longUppercase,
+    };
+
+String _formatQuantity({
+  required Unit outputUnit,
+  required num outputQuantity,
+  required QuantityDisplayMode quantityDisplayMode,
+}) =>
+    switch (quantityDisplayMode) {
+      FixedPrecisionDisplayMode(precision: final precision) =>
+        outputQuantity.toStringAsFixed(precision),
+      PrecisionDisplayMode(precision: final precision) =>
+        outputQuantity.toStringAsPrecision(precision),
+      TruncateDisplayMode() => outputQuantity.truncate().toString(),
+      RoundDisplayMode() => outputQuantity.round().toString(),
+      CustomQuantityDisplayMode(converter: final converter) =>
+        converter(outputQuantity, unit: outputUnit),
+    };
