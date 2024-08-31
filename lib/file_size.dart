e2cfcfb508d2643318d86ea27bda931be3918e6c
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-08-31 13:24:21 +0100

    feat: Add exports.

--- lib/file_size.dart
+++ lib/file_size.dart
@@ -1,2 +1,7 @@
 /// Get a human representation of the size of your files.
 library;
+
+export 'src/file_size.dart';
+export 'src/quantity_display_mode.dart';
+export 'src/unit.dart';
+export 'src/unit_conversion.dart';
