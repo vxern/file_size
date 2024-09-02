b9906fd12882b3718be28d78edc0bef8fcf229d0
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-09-02 21:07:07 +0100

    feat: Remove `FixedPrecisionDisplayMode` and `PrecisionDisplayMode`.

--- lib/src/quantity_display_mode.dart
+++ lib/src/quantity_display_mode.dart
@@ -45,43 +45,6 @@ class SimpleDisplayMode extends QuantityDisplayMode {
   }
 }
 
-/// The quantity is displayed with a fixed precision.
-///
-/// For example, with a [precision] of 3, a quantity 10.93214 will be displayed
-/// as '10.932', meanwhile 10 will be displayed as '10.000'.
-class FixedPrecisionDisplayMode extends QuantityDisplayMode {
-  /// The fixed precision to display the quantity with.
-  ///
-  /// The value must be between 0 (inclusive) and 20 (inclusive).
-  final int precision;
-
-  /// Returns an instance of [FixedPrecisionDisplayMode].
-  const FixedPrecisionDisplayMode({this.precision = 1});
-
-  @override
-  String format(num quantity, {required Unit unit}) =>
-      quantity.toStringAsFixed(precision);
-}
-
-/// The quantity is displayed with a set precision.
-///
-/// For example, with a [precision] of 3, a quantity 10.93214 will be displayed
-/// as '10.932', a quantity 10.9 will be displayed as '10.9', and a quantity of
-/// 10 will be displayed as just '10'.
-class PrecisionDisplayMode extends QuantityDisplayMode {
-  /// The precision to display the quantity with.
-  ///
-  /// The value must be between 1 (inclusive) and 20 (inclusive).
-  final int precision;
-
-  /// Returns an instance of [PrecisionDisplayMode].
-  const PrecisionDisplayMode({this.precision = 1});
-
-  @override
-  String format(num quantity, {required Unit unit}) =>
-      quantity.toStringAsPrecision(precision);
-}
-
 /// A function used to format a [quantity] based on its [unit].
 typedef QuantityFormatter = String Function(num quantity, {required Unit unit});
 
