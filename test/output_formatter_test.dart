3c5417f0cc011a7094877fd40495425fc7e42c2f
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-11-20 20:56:17 +0000

    feat: Add tests.

--- test/output_formatter_test.dart
+++ test/output_formatter_test.dart
@@ -5,12 +5,35 @@ void main() {
   group('SimpleOutputFormatter', () {
     group('format()', () {
       test(
-        "formats the parameters as '{quantity} {unit}'",
+        "formats the parameters as '{quantity} {unit}'.",
         () {
           expect(
             const SimpleOutputFormatter()
                 .format(quantity: '123', unit: 'units'),
             equals('123 units'),
+          );
+        },
+      );
+
+      test(
+        'if [includeSpace] is enabled, inserts a space between the quantity '
+        'and unit.',
+        () {
+          expect(
+            const SimpleOutputFormatter().format(quantity: '1', unit: 'b'),
+            equals('1 b'),
+          );
+        },
+      );
+
+      test(
+        'if [unitFirst] is enabled, inserts a space between the quantity '
+        'and unit.',
+        () {
+          expect(
+            const SimpleOutputFormatter(unitFirst: true)
+                .format(quantity: '1', unit: 'b'),
+            equals('b 1'),
           );
         },
       );
