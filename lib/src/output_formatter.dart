aa4a90af9e8d3f2baaf8041c3da9d2160500db30
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-11-20 20:47:23 +0000

    feat: Add options `includeSpace` and `unitFirst` to `SimpleOutputFormatter`.

--- lib/src/output_formatter.dart
+++ lib/src/output_formatter.dart
@@ -12,10 +12,22 @@ abstract class OutputFormatter {
 
 /// The quantity and unit are formatted as '${quantity} ${unit}'.
 class SimpleOutputFormatter extends OutputFormatter {
+  /// If enabled, there will be a space in-between the quantity and unit.
+  final bool includeSpace;
+
+  /// If enabled, the unit will come before the quantity.
+  final bool unitFirst;
+
   /// Returns an instance of [SimpleOutputFormatter].
-  const SimpleOutputFormatter();
+  const SimpleOutputFormatter({
+    this.includeSpace = true,
+    this.unitFirst = false,
+  });
 
   @override
-  String format({required String quantity, required String unit}) =>
-      '$quantity $unit';
+  String format({required String quantity, required String unit}) {
+    final (first, second) = unitFirst ? (unit, quantity) : (quantity, unit);
+
+    return [first, if (includeSpace) '', second].join();
+  }
 }
