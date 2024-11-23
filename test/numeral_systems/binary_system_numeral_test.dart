40c7d83a0fdfa7165dd3ca6347c4af0a7e3f9d0d
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-11-23 13:35:53 +0000

    refactor: Make tests more concise.

--- test/numeral_systems/binary_system_numeral_test.dart
+++ test/numeral_systems/binary_system_numeral_test.dart
@@ -1,37 +1,7 @@
-import 'package:collection/collection.dart';
 import 'package:human_file_size/src/numeral_systems/binary_numeral_system.dart';
-import 'package:test/test.dart';
 
-import '../utils.dart';
+import 'numeral_system.dart';
 
 void main() {
-  group('units', () {
-    final units = const BinaryNumeralSystem().units;
-
-    test('there should be an even number of units.', () {
-      expect(units.length.isEven, isTrue);
-    });
-
-    test('the factor between every unit in pairs should be 8.', () {
-      final factors =
-          units.slices(2).map((pair) => getFactor(pair.first, pair.last));
-
-      expect(factors, everyElement(equals(8)));
-    });
-
-    test(
-      'the factor between every 2nd unit should be 1024.',
-      () {
-        final factors = [
-          units.whereIndexed((index, _) => index.isEven),
-          units.whereIndexed((index, _) => index.isOdd),
-        ]
-            .map((units) => units.toList().windows(2))
-            .flattened
-            .map((pair) => getFactor(pair.first, pair.last));
-
-        expect(factors, everyElement(equals(1024)));
-      },
-    );
-  });
+  testNumeralSystem(const BinaryNumeralSystem(), expectedUnitFactor: 1024);
 }
--- test/numeral_systems/decimal_system_numeral_test.dart
+++ test/numeral_systems/decimal_system_numeral_test.dart
@@ -1,37 +1,7 @@
-import 'package:collection/collection.dart';
 import 'package:human_file_size/src/numeral_systems/decimal_numeral_system.dart';
-import 'package:test/test.dart';
 
-import '../utils.dart';
+import 'numeral_system.dart';
 
 void main() {
-  group('units', () {
-    final units = const DecimalNumeralSystem().units;
-
-    test('there should be an even number of units.', () {
-      expect(units.length.isEven, isTrue);
-    });
-
-    test('the factor between every unit in pairs should be 8.', () {
-      final factors =
-          units.slices(2).map((pair) => getFactor(pair.first, pair.last));
-
-      expect(factors, everyElement(equals(8)));
-    });
-
-    test(
-      'the factor between every 2nd unit should be 1000.',
-      () {
-        final factors = [
-          units.whereIndexed((index, _) => index.isEven),
-          units.whereIndexed((index, _) => index.isOdd),
-        ]
-            .map((units) => units.toList().windows(2))
-            .flattened
-            .map((pair) => getFactor(pair.first, pair.last));
-
-        expect(factors, everyElement(equals(1000)));
-      },
-    );
-  });
+  testNumeralSystem(const DecimalNumeralSystem(), expectedUnitFactor: 1000);
 }
